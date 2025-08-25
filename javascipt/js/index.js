2e39246a122a256c4f0b42b765d513e902977760
nishtha-thakkar <nishthathakkar3@gmail.com>
2025-08-25 14:09:13 +0530

    variables&types of data peimitive data

---
+++ javascipt/js/index.js
@@ -0,0 +1,76 @@
+// today's topic
+
+// what is variable , understanding data types , declaring and intializing variabls
+
+// variable meaning; is a container which has name and store tge value ,name dete hai and uske andar hum value store krte hai
+
+
+// heroName,userName(camel case)
+// full_name, mobile_name (snake case)
+
+// let is a keyword in which we save the name
+
+// varibale name should be diffrent. like variable is princessName , wecan not use second time this name
+
+let princessName = "Barbi"
+console.log(princessName);
+
+let heroName = "varundhawan"
+console.log(heroName)
+
+// types of Data
+//     there are two types of Data
+//     1.primitive Data
+//     2.non-primitive Data
+
+//     1. primitive Data
+//     Number
+//     String
+//     Boolean
+//     undefine
+//     null
+//     big int
+
+
+// 1.number
+
+let num1 = 12;
+console.log(num1)
+console.log(typeof num1)
+
+// 2.string means name
+
+let actorName = "helishah"
+console.log(actorName);
+console.log(typeof actorName);
+
+// 3.Bollen
+//  in Bollen have only 2 option 1.true 2. false yeh muje follow krta hai toh true nhi krta hai toh false
+
+
+let isFollow = true
+console.log(isFollow);
+console.log(typeof isFollow);
+
+// 4.undefine
+    // in undefine wecan not initialize any value so i will get undefine value
+
+    let studentName ;
+    console.log(studentName);
+    console.log(typeof studentName);
+
+//  5.null
+//  absent ofan object (data type is object)   
+
+let price = null;
+console.log(price);
+console.log(typeof price);
+
+// 6. big int
+//    to store big value and whenever you store big vlaue you should use n after value , you don't use n so it can not give specific value
+
+let bigValue = 123412341234456789075n;
+console.log(bigValue);
+console.log(typeof bigValue);
+
+
